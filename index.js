d0a6d51458fda93b1ce49b4aeb9b071524ca095d
fabiola barrios <fabi.barrios23@gmail.com>
2022-07-06 23:12:30 -0300

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,114 @@
-import { Container } from './contenedor.js';
 import express from 'express';
+import fs from 'fs'
+
 const app = express();
 
+class Container {
+
+    constructor(file) {
+        this.file = file;
+    }
+
+    async save(object) {
+        try {
+
+            if (!fs.existsSync(this.file)) {
+                fs.writeFileSync(this.file, "[]")
+            }
+            let products = await fs.promises.readFile(this.file, 'utf-8')
+            if (!products) {
+                object = {id:1, ...object}
+                products = [object]
+                await fs.promises.writeFile(this.file, JSON.stringify(products))
+                return 1
+            }
+            else {
+                products = JSON.parse(products)
+                const idObject = products[products.length-1].id+1
+                object = {id:idObject, ...object}
+                products = [...products,object]
+                await fs.promises.writeFile(this.file, JSON.stringify(products))
+                return idObject
+
+            }
+
+
+
+
+        } catch (error) {
+            console.log('Error al guardar el producto en save():', error)
+        }
+    }
+
+
+
+    async getAll() {
+        try {
+            const products = await fs.promises.readFile(this.file, 'utf-8')
+            const productsParsed = JSON.parse(products)
+            return productsParsed
+        } catch (error) {
+            console.log('Error de lectura en getAll():', error)
+        }
+    }
+
+
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(this.file,"[]")
+    
+        } catch (error) {
+            console.log('Error al borrar productos en deleteAll():', error)
+            
+        }
+    }
+
+
+    async getById(id) {
+        try {
+            let products = await this.getAll()
+            let foundProduct = products.find(product => {
+                return product.id==id
+            })
+            if(foundProduct){
+                return foundProduct
+
+            }
+            else {
+                console.log("no se encontró el producto")
+                return null
+            }
+
+
+            
+        } catch (error) {
+            console.log('Error al obtener producto en getById():', error)
+            
+        }
+
+    }
+    
+
+    async deleteById(id) {
+        try {
+            let products = await this.getAll()
+            let productToDelete = await this.getById(id)
+
+            products = products.filter(product => {
+                return product.id != productToDelete.id
+            })
+
+            await fs.promises.writeFile(this.file,JSON.stringify(products))
+            
+        } catch (error) {
+            console.log('Error al eliminar producto en deleteById():', error)
+            
+        }
+
+    }
+
+
+}
 
 
 
